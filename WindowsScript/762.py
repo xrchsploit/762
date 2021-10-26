5b0f4a1f36f86683a5dce5a797ff7ee2042bacf1
xrchsploit <54417402+xrchsploit@users.noreply.github.com>
2021-10-25 20:06:09 -0700

    Add files via upload

--- WindowsScript/762.py
+++ WindowsScript/762.py
@@ -65,17 +65,101 @@ Enable HTTPS only mode
 Script will stop for 15 seconds for you to do this.
 """ + bcolors.NORMAL)
 
-time(sleep 15)
+time.sleep(15)
 
 print(bcolors.RED + 
-"""Starting password complecity settings... 
+"""Starting password complexity settings... 
 Maximum Password Age -> 30
 Minimum Password Age -> 10
 Minimum Password Length -> 12
+Lockout Duration -> 30
+Lockout Threshold -> 5
+Lockout Window -> 10
+
+
 """ + bcolors.NORMAL)
 
 os.system("net accounts /maxpwage:30")
 os.system("net accounts /minpwage:10")
 os.system("net accounts /minpwlen:12")
+os.system("net accounts /lockoutduration:30")
+os.system("net accounts /lockoutthreshold:5")
+os.system("net accounts /lockoutwindow:10")
 
+# Notepad function
+os.system("cls")
+
+print(bcolors.BLUE + 
+"""A Notepad will open, set the options and be sure to not delete something on accident.
+
+PasswordComplexity = 1
+LockoutBadCount = 5
+ResetLockoutCount = 10
+RequireLogonToChangePassword = 1
+EnableGuestAccount = 0
+
+AuditSystemEvents = 1
+AuditLogonEvents = 1
+AuditObjectAccess = 1
+AuditPrivilegeUse = 1
+AuditPolicyChange = 1
+AuditAccountManage = 1
+AuditProcessTracking = 1
+AuditAccountLogon = 1
+
+
+!!! DO NOT FORGET TO SAVE THE CHANGES !!!
+""" + bcolors.NORMAL)
+
+time.sleep(3)
+
+os.system("secedit.exe /export /cfg C:\secconfig.cfg")
+os.system("start notepad C:\secconfig.cfg")
+os.system("secedit.exe /configure /db %windir%\securitynew.sdb /cfg C:\secconfig.cfg /areas SECURITYPOLICY")
+
+os.system("cls")
+
+print("""
+Now you have to manually audit using secpol.msc
+
+It will open up, go to the Local Policies >> Security Options and make sure everything looks good.
+""")
+
+time.sleep(3)
+os.system("cls")
+
+print("""
+Ok now you have to analyze the firewall settings in secpol.msc
+
+It should open, go to Windows Defender Firewall with Advanced Security >> The firewall
+Now on the right hand side click firewall properties
+
+1. Turn on the firewall
+2. Reject incoming
+3. Allow outgoing
+
+Now check the Connection Security Rules and make sure theres no sus rules in place
+Then check Inbound Rules and Outboud Rules 
+""")
+
+
+time.sleep(5)
+os.system("cls")
+print("This concludes the Secpol.msc audits...")
+
+# Done with Account Auditing
+
+print("Moving on to editing gpedit.msc *Its much easier to audit using gui*")
+time.sleep(3)
+os.system("cls")
+print("""
+When the editor opens go to User configuration >> Administrative Templates >> All Settings
+(If this isnt an option good luck)
+
+Go through and disable things that need to be disabled
+""")
+
+time.sleep(3)
+
+os.system("gpedit.msc")
 
